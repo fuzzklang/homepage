ae2557d91e3ae9a69b14d674d3062b98ec504736
Terje Sylvarnes <fuzzklang@gmail.com>
2024-10-26 12:43:14 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<div class="pageContainer">
+	<div class="main">
+		<h1 class="title">Welcome to my page</h1>
+		<section class="section">
+			<p>It is really nice to see you here</p>
+		</section>
+	</div>
+</div>
+
+<style>
+	.pageContainer {
+		display: flex;
+		justify-content: center;
+		font-family: 'Courier New', Courier, monospace;
+		background-color: rgb(39, 59, 59);
+		color: rgb(228, 228, 228);
+		width: 100%;
+		height: 100vh;
+	}
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		width: 960px;
+	}
+
+	.main > * {
+		align-self: center;
+	}
+
+	.title {
+		display: flex;
+		flex-direction: column;
+		padding-top: 3em;
+		padding-bottom: 1em;
+	}
+
+	.section {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+	}
+</style>
